355f183e8b2e65d206dd36e193237d86fbcb84c5
Sander Neele <scur2334@int4.local.snellius.surf.nl>
2026-01-12 18:52:08 +0100

    Question 7 code

--- src/ml_core/data/loader.py
+++ src/ml_core/data/loader.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 
 from .pcam import PCAMDataset
-
+import torch
 
 def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     """
@@ -16,15 +16,35 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     base_path = Path(data_cfg["data_path"])
 
     # TODO: Define Transforms
-    # train_transform = ...
-    # val_transform = ...
+    train_transform = None
+    val_transform = None
 
     # TODO: Define Paths for X and Y (train and val)
-    
+    x_train = base_path / "camelyonpatch_level_2_split_train_x.h5"
+    y_train = base_path / "camelyonpatch_level_2_split_train_y.h5"
+    x_val = base_path / "camelyonpatch_level_2_split_valid_x.h5"
+    y_val = base_path / "camelyonpatch_level_2_split_valid_y.h5"
+
     # TODO: Instantiate PCAMDataset for train and val
+    train_dataset = PCAMDataset(x_train, y_train, transform=train_transform, filter_data=False)
+    val_dataset = PCAMDataset(x_val, y_val, transform=val_transform, filter_data=False)
 
     # TODO: Create DataLoaders
-    # train_loader = ...
-    # val_loader = ...
+    batch = data_cfg["batch_size"]
+    totalworkers = data_cfg.get("num_workers", 0)
+    labellst = []
+    for i in range(len(train_dataset)):
+        _, y = train_dataset[i]
+        labellst.append(int(y))
+    labels = torch.tensor(labellst)
+    totalclasses = torch.bincount(labels)
+    weightclass = 1.0 / totalclasses.float()
+    weightsamples = weightclass[labels]
+    samplers = torch.utils.data.WeightedRandomSampler(weights=weightsamples, num_samples=len(weightsamples), 
+                                                      replacement=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch,sampler=samplers,
+                                             num_workers=totalworkers,)
+    val_loader = DataLoader(val_dataset, batch_size=batch, sampler=samplers,
+                                            num_workers=totalworkers)
     
-    raise NotImplementedError("Implement get_dataloaders")
+    return train_loader, val_loader
--- src/ml_core/data/pcam.py
+++ src/ml_core/data/pcam.py
@@ -12,20 +12,38 @@ class PCAMDataset(Dataset):
     PatchCamelyon (PCAM) Dataset reader for H5 format.
     """
 
-    def __init__(self, x_path: str, y_path: str, transform: Optional[Callable] = None):
+    def __init__(self, x_path: str, y_path: str, transform: Optional[Callable] = None, filter_data: bool = False):
         self.x_path = Path(x_path)
         self.y_path = Path(y_path)
         self.transform = transform
+        self.filter_data = filter_data
+        self.indices = None
+
 
         # TODO: Initialize dataset
         # 1. Check if files exist
         # 2. Open h5 files in read mode
-        pass
+        if not self.x_path.exists():
+            raise FileNotFoundError(f"File X has not been found: {self.x_path}")
+        if not self.y_path.exists():
+            raise FileNotFoundError(f"File Y has not been found: {self.y_path}")
+        with h5py.File(self.x_path, "r") as file:
+            self.length = len(file["x"])
+        if self.filter_data:
+            valid_lst = []  
+            with h5py.File(self.x_path, "r") as file:
+                for i in range(self.length):
+                    img = file["x"][i]
+                    m = img.mean()
+                    if 5 < m < 250:
+                        valid_lst.append(i)
+            self.indices = valid_lst
+            self.length = len(self.indices)
 
     def __len__(self) -> int:
         # TODO: Return length of dataset
         # The dataloader will know hence how many batches to create
-        return 0
+        return self.length
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         # TODO: Implement data retrieval
@@ -33,5 +51,20 @@ class PCAMDataset(Dataset):
         # 2. Convert to uint8 (for PIL compatibility if using transforms)
         # 3. Apply transforms if they exist
         # 4. Return tensor image and label (as long)
+        if self.indices is not None:
+            idx = self.indices[idx]
+        with h5py.File(self.x_path, "r") as filex:
+            image = filex["x"][idx]
+        with h5py.File(self.y_path, "r") as filey:
+            label = filey["y"][idx]
+
+        image = torch.from_numpy(image).float()
+        image = torch.clamp(image, 0, 255)
         
-        raise NotImplementedError("Implement __getitem__ in PCAMDataset")
+        image = image.permute(2, 0, 1) / 255.0
+
+        if self.transform is not None:
+            image = self.transform(image)
+        label = torch.tensor(label, dtype=torch.long).squeeze()
+        return image, label
+
