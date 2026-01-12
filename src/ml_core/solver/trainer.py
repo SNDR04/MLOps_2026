42a78243c3772203205655180f6bcbd9e68a8a07
Sander Neele <scur2334@int4.local.snellius.surf.nl>
2026-01-12 23:33:59 +0100

    trainerq9 added

--- src/ml_core/solver/trainer.py
+++ src/ml_core/solver/trainer.py
@@ -23,16 +23,16 @@ class Trainer:
         self.device = device
         
         # TODO: Define Loss Function (Criterion)
-        self.criterion = None
+        self.criterion = nn.CrossEntropyLoss()
 
         # TODO: Initialize ExperimentTracker
-        self.tracker = None
+        self.tracker = ExperimentTracker()
         
         # TODO: Initialize metric calculation (like accuracy/f1-score) if needed
-
+        self.logger = setup_logger()
     def train_epoch(self, dataloader: DataLoader, epoch_idx: int) -> Tuple[float, float, float]:
         self.model.train()
-        
+        loss = 0
         # TODO: Implement Training Loop
         # 1. Iterate over dataloader
         # 2. Move data to device
@@ -40,7 +40,18 @@ class Trainer:
         # 4. Backward pass, Optimizer step
         # 5. Track metrics (Loss, Accuracy, F1)
         
-        raise NotImplementedError("Implement train_epoch")
+        for i, j in tqdm(dataloader):
+            i = i.to(self.device)
+            j  = j.to(self.device)
+            self.optimizer.zero_grad()
+            outputs = self.model(i)
+            losss = self.criterion(outputs, j)
+            losss.backward()
+            self.optimizer.step()
+            loss += losss.item()
+        averageloss = loss /len(dataloader)
+        self.tracker.log_metric("train_loss", averageloss, step=epoch_idx)
+        return averageloss
 
     def validate(self, dataloader: DataLoader, epoch_idx: int) -> Tuple[float, float, float]:
         self.model.eval()
@@ -48,21 +59,43 @@ class Trainer:
         # TODO: Implement Validation Loop
         # Remember: No gradients needed here
         
-        raise NotImplementedError("Implement validate")
+        losstotal = 0
 
+        with torch.no_grad():
+            for i,j in dataloader:
+                i = i.to(self.device)
+                j  = j.to(self.device)
+                outputs = self.model(i)
+                loss = self.criterion(outputs, j)
+                losstotal += loss.item()
+        lossaverage = losstotal / len(dataloader)
+        self.tracker.log_metric("val_loss", lossaverage, step=epoch_idx)
+
+        return lossaverage
     def save_checkpoint(self, epoch: int, val_loss: float) -> None:
         # TODO: Save model state, optimizer state, and config
-        pass
+        checkpoint = {"epoch": epoch,"model_state_dict": self.model.state_dict(),"optimizer_state_dict": self.optimizer.state_dict(),
+        "val_loss": val_loss,"config": self.config,}
+
+        torch.save(checkpoint, f"checkpoint_epoch_{epoch}.pt")
 
     def fit(self, train_loader: DataLoader, val_loader: DataLoader) -> None:
         epochs = self.config["training"]["epochs"]
-        
+        lsttrainlosses = []
+        lst_val_losses = []
+
         print(f"Starting training for {epochs} epochs...")
         
         for epoch in range(epochs):
             # TODO: Call train_epoch and validate
             # TODO: Log metrics to tracker
             # TODO: Save checkpoints
-            pass
-            
+            train_loss = self.train_epoch(train_loader, epoch)
+            val_loss = self.validate(val_loader, epoch)
+            lsttrainlosses.append(train_loss)
+            lst_val_losses.append(val_loss)
+            print(f"Epoch {epoch+1}/{epochs}"
+                  f"Train Loss: {train_loss}"
+                  f"Val Loss: {val_loss}")
+        return lsttrainlosses, lst_val_losses
 	# Remember to handle the trackers properly
